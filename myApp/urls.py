b5737a08cbda0eb34e18748e9a5c182ff0f6d8dc
Temirlan504 <113613569+Temirlan504@users.noreply.github.com>
2023-04-20 18:21:01 -0400

    Added LessonDetailView
    Added LessonDetailView to view lesson itself. Will display questions using this view.
    Also, added path in the url, so it points to that view. Detail View requires pk, but bypassed and using str instead now

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CourseListView, LessonListView
+from .views import CourseListView, LessonListView, LessonDetailView
 
 app_name = "myApp"
 
 urlpatterns = [
     path('', CourseListView.as_view(), name="course-list"),
-    path('<slug:course_name>/', LessonListView.as_view(), name="lesson-list")
+    path('<str:course_name>/', LessonListView.as_view(), name="lesson-list"),
+    path('<str:course_name>/<str:lesson_name>/', LessonDetailView.as_view(), name="lesson-detail")
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,4 +1,4 @@
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from .models import Course, Lesson
 
 class CourseListView(ListView):
@@ -15,3 +15,25 @@ class LessonListView(ListView):
         course_name = self.kwargs['course_name'] # Get the selected course from the URL parameter
         lessons = Lesson.objects.filter(course__course_name=course_name) # Filter the lessons queryset by the selected course
         return lessons
+    
+    # Passing in additional arguments to the template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["course_name"] = self.kwargs['course_name']
+        return context
+    
+class LessonDetailView(DetailView):
+    model = Lesson
+    template_name = "myApp/lesson_detail_view.html"
+
+    # Done to use str in the url instead of a pk
+    def get_object(self):
+        course_name = self.kwargs['course_name']
+        lesson_name = self.kwargs['lesson_name']
+        lesson = Lesson.objects.get(course__course_name=course_name, lesson_name=lesson_name)
+        return lesson
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["lesson_name"] = self.kwargs['lesson_name']
+        return context
